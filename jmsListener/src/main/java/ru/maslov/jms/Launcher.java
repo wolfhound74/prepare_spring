1087779f912c9edb32ff1b10399c3864d88ba64b
RipperRoo <ripper_roo@rambler.ru>
2013-02-17 23:00:53 +0600

    jms listener

---
+++ jmsListener/src/main/java/ru/maslov/jms/Launcher.java
@@ -0,0 +1,10 @@
+package ru.maslov.jms;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Launcher {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:applicationContextJmsServer.xml");
+    }
+}
--- jmsListener/src/main/java/ru/maslov/jms/MyJmsMessageListener.java
+++ jmsListener/src/main/java/ru/maslov/jms/MyJmsMessageListener.java
@@ -1,15 +1,14 @@
-package ru.maslov.contactmanager.service.jms;
+package ru.maslov.jms;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
-public class JmsMessageListener implements MessageListener {
-    private static final Logger logger = LoggerFactory.getLogger(JmsMessageListener.class);
+public class MyJmsMessageListener implements javax.jms.MessageListener {
+    private static final Logger logger = LoggerFactory.getLogger(MyJmsMessageListener.class);
 
     @Override
     public void onMessage(Message message) {
@@ -18,7 +17,7 @@ public class JmsMessageListener implements MessageListener {
             if (message instanceof TextMessage) {
                 TextMessage tm = (TextMessage) message;
                 String msg = tm.getText();
-                System.out.println("\n------------\n" + msg);
+                System.out.println("\n----------------\n" + msg);
             }
         } catch (JMSException e) {
             logger.error(e.getMessage(), e);
