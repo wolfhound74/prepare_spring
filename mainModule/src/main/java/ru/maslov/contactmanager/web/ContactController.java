b1af92ec10dae4d07fc8b22e6b289af5dc7899a3
RipperRoo <ripper_roo@rambler.ru>
2013-02-24 18:01:08 +0600

    вызов session в контроллере

--- mainModule/src/main/java/ru/maslov/contactmanager/web/ContactController.java
+++ mainModule/src/main/java/ru/maslov/contactmanager/web/ContactController.java
@@ -7,12 +7,15 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.maslov.contactmanager.domain.Contact;
 import ru.maslov.contactmanager.service.ContactService;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
@@ -27,8 +30,8 @@ public class ContactController {
     @RequestMapping(value = "/create", method = RequestMethod.POST)
 //    @ResponseBody
     public String create(Contact contact, BindingResult bindingResult,
-                          Model uiModel, HttpServletRequest httpServletRequest,
-                          RedirectAttributes redirectAttributes, Locale locale) {
+                         Model uiModel, HttpServletRequest httpServletRequest,
+                         RedirectAttributes redirectAttributes, Locale locale) {
         contactService.save(contact);
         return "redirect:/contact/list";
     }
@@ -47,4 +50,10 @@ public class ContactController {
 //        map.put("object1",1);
         return contact;
     }
+
+    public static HttpSession session() {
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        return attr.getRequest().getSession(true); // true == allow create
+    }
+
 }
